e683d0042f8c27cce9eb02cbc101e3d0d97de09c
ISM43LL <ismael_rostirola@estudante.sc.senai.br>
2025-05-05 12:37:34 -0300

    a

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ import './Footer.css';
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>&copy;Todos os Direitos Reservados 2025.</p>
+      <p>Catálogo Filmes &copy;Todos os Direitos Reservados 2025.</p>
       <img src={logo} alt="Logótipo pequeno" className="footer-logo" />
     </footer>
   );
